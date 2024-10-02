4df0f640b87d406e1aedac4d9d0c0948a9c9ef0b
unknown <annie.challco@untrm.edu.pe>
2024-10-02 17:38:54 -0500

    first commit

---
+++ clase_2.R
@@ -0,0 +1,21 @@
+library(readxl)
+LA_MOLINA_2014_POTATO_WUE_FB_ <- read_excel("LA MOLINA 2014 POTATO WUE (FB).xlsx", 
+                                            sheet = "fb")
+View(LA_MOLINA_2014_POTATO_WUE_FB_)
+View(fb)
+
+
+library(readxl)
+LA_MOLINA_2014_POTATO_WUE_FB_ <- read_excel("LA MOLINA 2014 POTATO WUE (FB).xlsx", 
+                                            sheet = "fb")
+View(LA_MOLINA_2014_POTATO_WUE_FB_)
+
+library(readxl)
+LA_MOLINA_2014_POTATO_WUE_FB_ <- read_excel("LA MOLINA 2014 POTATO WUE (FB).xlsx", 
+                                            sheet = "fb")
+View(LA_MOLINA_2014_POTATO_WUE_FB_)
+
+
+
+str(fb)
+str(LA_MOLINA_2014_POTATO_WUE_FB_)
