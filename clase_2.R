c883944e21eae85848b7fc2988e69ceda35eb533
unknown <annie.challco@untrm.edu.pe>
2024-10-05 23:11:57 -0500

    analisi_datos

--- clase_2.R
+++ clase_2.R
@@ -1,21 +1,10 @@
-library(readxl)
-LA_MOLINA_2014_POTATO_WUE_FB_ <- read_excel("LA MOLINA 2014 POTATO WUE (FB).xlsx", 
-                                            sheet = "fb")
-View(LA_MOLINA_2014_POTATO_WUE_FB_)
-View(fb)
-
-
-library(readxl)
-LA_MOLINA_2014_POTATO_WUE_FB_ <- read_excel("LA MOLINA 2014 POTATO WUE (FB).xlsx", 
-                                            sheet = "fb")
-View(LA_MOLINA_2014_POTATO_WUE_FB_)
+install.packages("agricolae")
+library(agricolae)
+remove.packages("agricolae")  # Para desinstalar
+install.packages("agricolae")  # Para reinstalar
+R.version.string
+quarto::quarto_version()
 
 library(readxl)
-LA_MOLINA_2014_POTATO_WUE_FB_ <- read_excel("LA MOLINA 2014 POTATO WUE (FB).xlsx", 
-                                            sheet = "fb")
+LA_MOLINA_2014_POTATO_WUE_FB_ <- read_excel("LA MOLINA 2014 POTATO WUE (FB).xlsx")
 View(LA_MOLINA_2014_POTATO_WUE_FB_)
-
-
-
-str(fb)
-str(LA_MOLINA_2014_POTATO_WUE_FB_)
